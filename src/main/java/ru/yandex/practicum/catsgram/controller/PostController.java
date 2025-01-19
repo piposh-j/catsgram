f62ead97ec3164697c927b510ef42d140e2adbb9
Станислав С <4rmor3d@gmail.com>
2025-01-19 17:11:00 +0300

    Добавлены обработчики усключений

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.catsgram.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import ru.yandex.practicum.catsgram.enums.SortOrder;
+import ru.yandex.practicum.catsgram.exception.ParameterNotValidException;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
@@ -25,6 +27,17 @@ public class PostController {
             @RequestParam(defaultValue = "0") Long from,
             @RequestParam(defaultValue = "10") Long size
     ) {
+        if( SortOrder.from(sort) == null) {
+            throw new ParameterNotValidException("sort", "Получено: " + sort + " должно быть: ask или desc");
+        }
+
+        if( size <= 0 ) {
+            throw new ParameterNotValidException("size", "Размер должен быть больше нуля");
+        }
+        if( from < 0 ) {
+            throw new ParameterNotValidException("from", "Начало выборки должно быть положительным числом");
+        }
+
         return postService.findAll(sort, from, size);
     }
 
